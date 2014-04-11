c7f24897bef4febf0dc19433b20c65071b5d83ff
Danny Loomeijer <danny.loomeijer@fayntic.com>
2014-04-11 15:08:49 +0200

    Implemented the (payment) Ideal class

--- src/Sisow/API/Payment/Ideal.php
+++ src/Sisow/API/Payment/Ideal.php
@@ -2,7 +2,38 @@
 
 namespace Sisow\API\Payment;
 
-class Ideal
-{
+use Sisow\API\Client;
+use Sisow\API\Method\DirectoryRequest;
+use Sisow\API\Payment;
 
+class Ideal extends Payment
+{
+    /** @var int */
+    private $issuerId;
+
+    /**
+     * @param Client $client
+     * @return array
+     */
+    public function getAvailableIssuers(Client $client)
+    {
+        $directoryRequest = new DirectoryRequest($client);
+        return $directoryRequest->getAvailableIssuers();
+    }
+
+    /**
+     * @return int
+     */
+    public function getIssuerId()
+    {
+        return $this->issuerId;
+    }
+
+    /**
+     * @param int $issuerId
+     */
+    public function setIssuerId($issuerId)
+    {
+        $this->issuerId = $issuerId;
+    }
 } 
