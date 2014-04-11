4d2e52c704246221ad8d8a3e7894e26d6d6b7615
Danny Loomeijer <danny.loomeijer@fayntic.com>
2014-04-11 15:07:35 +0200

    Implemented the DirectoryRequest class

--- src/Sisow/API/Method/DirectoryRequest.php
+++ src/Sisow/API/Method/DirectoryRequest.php
@@ -2,9 +2,24 @@
 
 namespace Sisow\API\Method;
 
+use Sisow\API\Exception;
 use Sisow\API\Method;
 
 class DirectoryRequest extends Method
 {
-
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public function getAvailableIssuers()
+    {
+        $requestResult = $this->request();
+        if (!isset($requestResult['issuer'])) {
+            throw new Exception('No Issuers could be found');
+        }
+        if (isset($requestResult['issuer']['issuerid'])) {
+            return array($requestResult['issuer']);
+        }
+        return $requestResult['issuer'];
+    }
 } 
