04ded3ae21ff9b05358a233a05d485a5912ad0a5
Danny Loomeijer <danny.loomeijer@fayntic.com>
2014-04-11 14:56:31 +0200

    Implemented the basic (abstract) Payment class

--- src/Sisow/API/Payment.php
+++ src/Sisow/API/Payment.php
@@ -4,5 +4,243 @@ namespace Sisow\API;
 
 abstract class Payment
 {
+    const CURRENCY_EUR = 'EUR';
+    const CURRENCY_USD = 'USD';
 
+    /** @var int */
+    private $amount;
+
+    /** @var string */
+    private $callbackUrl;
+
+    /** @var string */
+    private $cancelUrl;
+
+    /** @var string */
+    private $currency = self::CURRENCY_EUR;
+
+    /** @var string */
+    private $description;
+
+    /** @var string */
+    private $entranceCode;
+
+    /** @var string */
+    private $ipAddress;
+
+    /** @var string */
+    private $notificationUrl;
+
+    /** @var string|int */
+    private $purchaseId;
+
+    /** @var string */
+    private $returnUrl;
+
+    public function __construct()
+    {
+        if ($ipAddress = filter_input(INPUT_SERVER, 'REMOTE_ADDR', FILTER_VALIDATE_IP)) {
+            $this->ipAddress = $ipAddress;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param int $amount Het bedrag in centen
+     * @throws Exception
+     */
+    public function setAmount($amount)
+    {
+        if (!is_numeric($amount)) {
+            throw new Exception('The amount must be given in cents (integer)');
+        }
+        $this->amount = (int)$amount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCallbackUrl()
+    {
+        return $this->callbackUrl;
+    }
+
+    /**
+     * @param string $callbackUrl
+     * @throws \Exception
+     */
+    public function setCallbackUrl($callbackUrl)
+    {
+        if (!filter_var($callbackUrl, FILTER_VALIDATE_URL)) {
+            throw new \Exception('The callbackUrl must be a valid address (scheme included!)');
+        }
+        $this->callbackUrl = $callbackUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCancelUrl()
+    {
+        return $this->cancelUrl;
+    }
+
+    /**
+     * @param string $cancelUrl
+     * @throws \Exception
+     */
+    public function setCancelUrl($cancelUrl)
+    {
+        if (!filter_var($cancelUrl, FILTER_VALIDATE_URL)) {
+            throw new \Exception('The cancelUrl must be a valid address (scheme included!)');
+        }
+        $this->cancelUrl = $cancelUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency ISO 4217 valutacode (standaard EUR)
+     * @throws Exception
+     */
+    public function setCurrency($currency)
+    {
+        if (!in_array($currency, array(self::CURRENCY_EUR, self::CURRENCY_USD))) {
+            throw new Exception('Currency must be one of the \Sisow\API\Payment::CURRENCY_ constants');
+        }
+        $this->currency = $currency;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @throws Exception
+     */
+    public function setDescription($description)
+    {
+        if (strlen($description) > 32) {
+            throw new Exception('The description may not be longer than 32 characters');
+        }
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntranceCode()
+    {
+        return $this->entranceCode;
+    }
+
+    /**
+     * @param string $entranceCode
+     * @throws Exception
+     */
+    public function setEntranceCode($entranceCode)
+    {
+        if (!preg_match('/^[0-9a-z]+$/i', $entranceCode) || strlen($entranceCode) > 40) {
+            throw new Exception('The EntranceCode may only contain [0-9a-z] and must not be longer than 40 characters');
+        }
+        $this->entranceCode = $entranceCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+
+    /**
+     * @param string $ipAddress
+     * @throws Exception
+     */
+    public function setIpAddress($ipAddress)
+    {
+        if (!filter_var($ipAddress, FILTER_VALIDATE_IP)) {
+            throw new Exception('The given IP address is not valid (must be either IPv4 or IPv6');
+        }
+        $this->ipAddress = $ipAddress;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNotificationUrl()
+    {
+        return $this->notificationUrl;
+    }
+
+    /**
+     * @param string $notificationUrl
+     * @throws \Exception
+     */
+    public function setNotificationUrl($notificationUrl)
+    {
+        if (!filter_var($notificationUrl, FILTER_VALIDATE_URL)) {
+            throw new \Exception('The notificationUrl must be a valid address (scheme included!)');
+        }
+        $this->notificationUrl = $notificationUrl;
+    }
+
+    /**
+     * @return string|int
+     */
+    public function getPurchaseId()
+    {
+        return $this->purchaseId;
+    }
+
+    /**
+     * @param string|int $purchaseId Het betalingskenmerk, maximaal 16 posities
+     * @throws Exception
+     */
+    public function setPurchaseId($purchaseId)
+    {
+        if (strlen($purchaseId) > 16) {
+            throw new Exception('The PurchaseId may not be longer than 16 characters');
+        }
+        $this->purchaseId = $purchaseId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReturnUrl()
+    {
+        return $this->returnUrl;
+    }
+
+    /**
+     * @param string $returnUrl
+     * @throws \Exception
+     */
+    public function setReturnUrl($returnUrl)
+    {
+        if (!filter_var($returnUrl, FILTER_VALIDATE_URL)) {
+            throw new \Exception('The returnUrl must be a valid address (scheme included!)');
+        }
+        $this->returnUrl = $returnUrl;
+    }
 } 
