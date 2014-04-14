8e2242dfafbddd893211313956bd59a59e72c8bb
Danny Loomeijer <danny.loomeijer@fayntic.com>
2014-04-14 15:53:05 +0200

    Fixed docblocks...

--- src/Sisow/API/Method/PingRequest.php
+++ src/Sisow/API/Method/PingRequest.php
@@ -2,11 +2,16 @@
 
 namespace Sisow\API\Method;
 
+use Sisow\API\Exception;
 use Sisow\API\Method;
 use Sisow\API\Result\PingRequestResult;
 
 class PingRequest extends Method
 {
+    /**
+     * @return PingRequestResult
+     * @throws Exception
+     */
     public function execute()
     {
         return new PingRequestResult($this->getClient(), parent::execute());
